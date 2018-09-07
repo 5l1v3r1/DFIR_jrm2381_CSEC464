1accfe537791d8cc09d28444a0dd6271a0d92fd5
jmaskelony <james.maskelony@gmail.com>
2018-09-06 21:11:16 -0400

    Add files via upload
    to remote into mult pcs

---
+++ ps_art_driver.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host "Press enter on a blank entry to finish"
+$comps = @()
+do{
+    $inp = Read-Host "Enter an IP address to connect to"
+    $comps += $inp
+} while ($inp -ne "")
+
+foreach($ip in $comps){
+    Invoke-Command -ComputerName $ip -FilePath C:\path\to\script
+}
